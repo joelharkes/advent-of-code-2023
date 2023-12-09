da942fec97692262bc689dcc1475fa4522466606
Joël Harkes <jlharkes@gmail.com>
2023-12-09 21:52:03 +0100

    refactored to matrix and cells

--- day3/solution.dart
+++ day3/solution.dart
@@ -1,7 +1,5 @@
 import 'dart:collection';
-
 import 'package:path/path.dart' show dirname, join;
-import 'package:string_extensions/string_extensions.dart';
 import 'package:collection/collection.dart';
 import 'dart:io' show File, Platform;
 
@@ -12,18 +10,13 @@ void main() {
   print(path);
   var file = File(join(dirname(path), 'input.txt'));
   var lines = file.readAsLinesSync();
-  var grid = lines.map((e) => e.split('').toList()).toList();
-  final uniquePartNumbers = HashSet<PartNumber>(
-    equals: (a, b) => a.x == b.x && a.y == b.y,
-    hashCode: (a) => a.x.hashCode + a.y.hashCode,
-  );
-  grid.forEachIndexed((y, row) {
-    row.forEachIndexed((x, element) {
-      if (parseSchematicType(element) == SchematicType.Part) {
-        var numbers = findNumbers(grid, x, y);
-        uniquePartNumbers.addAll(numbers);
-      }
-    });
+  var grid = new Matrix(lines.map((e) => e.split('').toList()).toList());
+  final uniquePartNumbers = UniquePartNumbers();
+  grid.forEachCell((coordinate, cell) {
+    if (parseSchematicType(cell) == SchematicType.Part) {
+      var numbers = findNumbers(grid, coordinate);
+      uniquePartNumbers.addAll(numbers);
+    }
   });
 
   var sumOfPartNumbers = uniquePartNumbers.map((parts) => parts.number).sum;
@@ -31,22 +24,15 @@ void main() {
 
   // part 2: find gears
   var gearRatios = <int>[];
-  grid.forEachIndexed((y, row) {
-    row.forEachIndexed((x, element) {
-      if (element == '*') {
-        var numbers = findNumbers(grid, x, y);
-        final uniquePartNumbers = HashSet<PartNumber>(
-          equals: (a, b) => a.x == b.x && a.y == b.y,
-          hashCode: (a) => a.x.hashCode + a.y.hashCode,
-        );
-        uniquePartNumbers.addAll(numbers);
-        if (uniquePartNumbers.length == 2) {
-          var gearRatio =
-              uniquePartNumbers.map((e) => e.number).reduce((a, b) => a * b);
-          gearRatios.add(gearRatio);
-        }
+  grid.forEachCell((coordinate, cell) {
+    if (cell == '*') {
+      var numbers = findNumbers(grid, coordinate);
+      // 2 numbers means its a gear
+      if (numbers.length == 2) {
+        var gearRatio = numbers.map((e) => e.number).reduce((a, b) => a * b);
+        gearRatios.add(gearRatio);
       }
-    });
+    }
   });
   var sumOfGearsRatios = gearRatios.sum;
   gearRatios.reduce((value, element) => value * element);
@@ -82,57 +68,119 @@ SchematicType parseSchematicType(String input) {
 
 class PartNumber {
   final int number;
-  final int x;
-  final int y;
-  PartNumber(this.number, this.x, this.y);
-}
+  final Coordinate coordinate;
+  PartNumber(this.number, this.coordinate);
 
-List<PartNumber> findNumbers(List<List<String>> grid, int x, int y) {
-  List<PartNumber> numbers = [];
-  for (var i = -1; i <= 1; i++) {
-    for (var j = -1; j <= 1; j++) {
-      var field = grid[y + i][x + j];
-      if (parseSchematicType(field) == SchematicType.PartNumber) {
-        numbers.add(findNumber(grid, x + j, y + i));
-      }
-    }
+  bool equals(PartNumber other) {
+    return this.coordinate.equals(other.coordinate);
   }
-  return numbers;
+
+  @override
+  int get hashCode => coordinate.hashCode;
 }
 
-PartNumber findNumber(List<List<String>> grid, int x, int y) {
-  var startX = x;
+UniquePartNumbersType findNumbers(Matrix<String> grid, Coordinate coordinate) {
+  var uniqueNumbers = UniquePartNumbers();
+  grid.getNeighbourCoordinates(coordinate).forEach((neighbourCoordinate) {
+    var cell = grid.getCell(neighbourCoordinate) ?? '.';
+    if (parseSchematicType(cell) == SchematicType.PartNumber) {
+      uniqueNumbers.add(findNumber(grid, neighbourCoordinate));
+    }
+  });
+  return uniqueNumbers;
+}
 
-  while (
-      parseSchematicType(grid.getCell(startX, y)) == SchematicType.PartNumber) {
+PartNumber findNumber(Matrix<String> grid, Coordinate coordinate) {
+  var startX = coordinate.x;
+
+  while (parseSchematicType(
+          (grid.getCell(Coordinate(startX, coordinate.y))) ?? '.') ==
+      SchematicType.PartNumber) {
     startX--;
   }
   startX++; // back one to far.
 
   var numberString = '';
   var currentX = startX;
-  while (parseSchematicType(grid.getCell(currentX, y)) ==
+  while (parseSchematicType(
+          (grid.getCell(Coordinate(currentX, coordinate.y)) ?? '.')) ==
       SchematicType.PartNumber) {
-    numberString += grid[y][currentX];
+    numberString += grid.getCell(Coordinate(currentX, coordinate.y)) as String;
     currentX++;
   }
   var value = int.parse(numberString);
-  return PartNumber(value, startX, y);
+  return PartNumber(value, Coordinate(startX, coordinate.y));
 }
 
-extension NumberParsing on List<List<String>> {
-  // major domain hack but im lazy :D
-  String getCell(x, y) {
+class Coordinate {
+  final int x;
+  final int y;
+  Coordinate(this.x, this.y);
+
+  bool equals(Coordinate other) {
+    return this.x == other.x && this.y == other.y;
+  }
+
+  @override
+  int get hashCode => x.hashCode + y.hashCode;
+}
+
+class Matrix<T> {
+  final List<List<T>> _matrix;
+  Matrix(this._matrix);
+
+  T? getCell(Coordinate coordinate) {
+    var x = coordinate.x;
+    var y = coordinate.y;
     if (x < 0 || y < 0) {
-      return '.';
+      return null;
     }
-    if (y >= this.length) {
-      return '.';
+    if (y >= this._matrix.length) {
+      return null;
     }
-    var row = this[y];
+    var row = this._matrix[y];
     if (x >= row.length) {
-      return '.';
+      return null;
     }
     return row[x];
   }
+
+  void forEachCell(void Function(Coordinate coordinate, T cell) callback) {
+    for (var y = 0; y < _matrix.length; y++) {
+      var row = _matrix[y];
+      for (var x = 0; x < row.length; x++) {
+        callback(Coordinate(x, y), row[x]);
+      }
+    }
+  }
+
+  List<Coordinate> getNeighbourCoordinates(Coordinate coordinate) {
+    var x = coordinate.x;
+    var y = coordinate.y;
+    var neighbours = <Coordinate>[];
+    for (var i = -1; i <= 1; i++) {
+      for (var j = -1; j <= 1; j++) {
+        if (i == 0 && j == 0) {
+          continue;
+        }
+        var cell = getCell(Coordinate(x + i, y + j));
+        if (cell != null) {
+          neighbours.add(Coordinate(x + i, y + j));
+        }
+      }
+    }
+    return neighbours;
+  }
+}
+
+typedef UniquePartNumbersType = HashSet<PartNumber>;
+UniquePartNumbersType UniquePartNumbers([List<PartNumber>? parts]) {
+  var set = HashSet<PartNumber>(
+    equals: (a, b) => a.equals(b),
+    hashCode: (a) => a.hashCode,
+  );
+  if (parts != null) {
+    set.addAll(parts);
+  }
+  return set;
 }
